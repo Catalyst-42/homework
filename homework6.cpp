d55e1097b7afd3df198ca576ae81696bb3a8838f
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2022-11-26 19:15:19 +0300

    hw6

--- homework6.cpp
+++ homework6.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <math.h>
@@ -8,12 +9,13 @@
 
 // #define M_PI 3.14159265358979323846
 #define R "\033[31m"
+#define G "\033[32m"
 #define Y "\033[33m"
 #define P "\033[35m"
 #define W "\033[0m"
 
 using namespace std;
-const char *clear = "clear";
+const char *clear = "clear"; // or "cls" for windows
 
 int counter = 0;
 void permutate(string& s, int l, int r) {
@@ -151,11 +153,127 @@ void t4() {
     cout << "\nВсего счастливых билетов: " << counter << " (" << counter/10000.0 << "%)" << '\n';
 }
 
+string toBinary(int n) {
+    string binary;
+    while (n != 0) {
+        binary = (n%2==0 ? '0' : '1') + binary;
+        n /= 2;
+    }
+    return binary;
+}
+
+void t5() {
+    cout << "В выражении 12894*4193*9510*8653*4381*2546*1158*8645*2587 заменить звёздочки знаками \"+\" или \"-\", чтобы получившееся выражение равнялось 1989\n\n";
+    string command_binary = "";
+    int sum = 12894;
+
+    int numbers[9] = {12894, 4193, 9510, 8653, 4381, 2546, 1158, 8645, 2587};
+    for (int command = 0; command < 0b11111111; ++command) {
+        // make 01010011 command, 0 -> -, 1 -> +
+        command_binary = "";
+        for (int _ = 0; _ < 8 - toBinary(command).length(); ++_) {
+            command_binary += '0';
+        }
+        command_binary += toBinary(command);
+
+        // try to count by command
+        sum = 12894;
+        for (int c = 0; c < 8; ++c) {
+            if (command_binary[c] == '0') sum -= numbers[c+1];
+            else sum += numbers[c+1];
+        }
+
+        if (sum == 1989) {
+            for (int c = 0; c < 8; ++c) {
+                cout << numbers[c] << ((command_binary[c] == '0') ? " - " : " + ");
+            }
+            cout << numbers[8] << " = " << sum;
+        }
+    }
+} 
+
+void t6() {
+    cout << "87. Построить все правильные скобочные выражения длины 10\n\n";
+    string str = "";
+    int sum = 0;
+    counter = 0;
+
+    for (int i=0; i<0b1111111111; ++i) {
+        str = "";
+        for (int _ = 0; _ < 10 - toBinary(i).length(); ++_) {
+            str += '0';
+        }
+        str += toBinary(i);
+        
+        // 0 -> ( 
+        // 1 -> )
+        replace(str.begin(), str.end(), '0', '(');
+        replace(str.begin(), str.end(), '1', ')');
+        sum = 0;
+        for (int j=0; j<10; ++j) {
+            if (str[j]=='(') sum++;
+            else sum--;
+            
+            if (sum < 0) break;
+            if (j == 9 && sum == 0) {
+                cout << str + ' ';
+                counter++;
+                if (counter % 5 == 0) cout << '\n';
+            }
+        }
+    }
+
+    cout << "\n\nВсего комбинаций: " << counter << '\n';
+}
+
+void t7() {
+    cout << "96. Игра \"быки и коровы\"\nПравила:\nПопытайтесь угадать комбинацию из 4 разных чисел за наименьшее число ходов\n+ -> угадано число и позиция\n- -> число есть в ответе\n";
+    string numbers = "0123456789";
+    string answer = "0000";
+    string guess = "";
+    int index = 0;
+    counter = 0;
+
+    // generate answer
+    for (int i=0; i<4; ++i) {
+        index = (double) rand()*numbers.length() / RAND_MAX;
+        answer[i] = numbers[index];
+        numbers.erase(index, 1);
+    }
+
+    // guess loop
+    while (guess != answer) {
+        cout << "\n> ";
+        cin >> guess;
+        
+        if (guess.length() == 4) {
+            counter++;
+
+            cout << "  ";
+            for (int i=0; i<4; ++i) {
+                if (guess[i] == answer[i]) cout << G << "+" << W;
+                else if (answer.find(guess[i], 0) != string::npos) cout << Y << "-" << W;
+                else cout << ' ';
+            }
+
+            cout << '\n';
+        } 
+        else if (guess == "0") {
+            return;
+        }
+        else {
+            cout << "Неправильный ввод!\n";
+        }
+    }
+
+    cout << "\nВерно!\nНа решение ушло ходов: " << counter << '\n';
+}
+
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    const int tasks = 4;
-    void (*functions[tasks])() = {t1, t2, t3, t4};
+    const int tasks = 7;
+    void (*functions[tasks])() = {t1, t2, t3, t4, t5, t6, t7};
     int function_number = 0;
 
     while (true) {
